c98bdadd34a097e25ebc5475d137bfd6281f413b
mikumikulch <mikumiku.lch@hotmail.com>
2017-07-07 16:33:47 +0800

    完善小助手获取考勤数据，并与昨日数据比较功能

--- helper_core_process.py
+++ helper_core_process.py
@@ -6,6 +6,8 @@
 """
 import json
 
+import logging
+
 from attendance_spider.LixinStaffInfoSpider import LixinStaffInfoSpider
 from datetime import datetime, timedelta, time
 
@@ -19,10 +21,10 @@ class HelperRobot(object):
     def compare_date(self, element):
         attendence_date = datetime.fromtimestamp(element).strftime('%Y-%m-%d')
         # 昨日的日期
-        now = datetime.now
+        now = datetime.now()
         yesterday = now - timedelta(days=1)
         formated_yesterday = yesterday.strftime('%Y-%m-%d')
-        return formated_yesterday is attendence_date
+        return formated_yesterday == attendence_date
 
     def engin_start(self):
         # 抓取当月考勤记录
@@ -31,14 +33,26 @@ class HelperRobot(object):
         # 获取当月考勤记录的 dict 数据
         attendance_dict = staff_attendance_info_json['data']['checkDay']
         # 获取记录的日期数据列表
-        check_day_list = [k for k in attendance_dict.items()]
+        check_day_list = [k for k, v in attendance_dict.items()]
         # 过滤出考勤日期中不是昨日的日期的数据
-        yesterday_attendance_date = filter(self.compare_date, map(lambda x: float(x), check_day_list))[0]
+        int_check_day_list = list(map(lambda x: int(x), check_day_list))
+        yesterday_attendance_date = list(filter(self.compare_date, int_check_day_list))[0]
         # 获取打卡记录
-        checkin_attr_dict = attendance_dict[yesterday_attendance_date]['checkin_attr']
-        checkout_attr_dict = attendance_dict[yesterday_attendance_date]['checkout_attr_dict']
-        # TODO trycatch 处理转换异常，未打卡时表示请假或者缺勤，处理结束
-        checkout_time = time.strftime(checkout_attr_dict['time'], '%H:%M:%S')
+        checkin_attr_dict = attendance_dict[str(yesterday_attendance_date)][0]['checkout_attr']
+        checkout_attr_dict = attendance_dict[str(yesterday_attendance_date)][0]['checkout_attr']
+        try:
+            checkout_time = datetime.strptime(checkout_attr_dict['time'], '%H:%M:%S')
+        except BaseException:
+            logging.error('您的退勤打卡时间有异常。请确认您的打卡信息。退勤打卡时间：%s', checkout_attr_dict['time'])
+            return
+        # 判断打卡时间是否超过8点。如果超过8点，调用打印系统打印加班单。
+        criterion_time = datetime(1900, 1, 1, 20, 0, 0)
+        if checkout_time >= criterion_time:
+            # 当天符合加班标准，调用打印机模块
+            pass
+        else:
+            yesterday = datetime.fromtimestamp(yesterday_attendance_date).strftime('%Y-%m-%d')
+            logging.info('您昨日的考勤时间 %s 未满足加班条件。程序处理结束', yesterday)
 
 
 robot = HelperRobot()
